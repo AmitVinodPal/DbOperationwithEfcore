3a2ebc6e9a6e6d81ff211cd8e76362012c1effad
Amit231999 <157674058+Amit231999@users.noreply.github.com>
2025-09-06 12:57:19 +0530

    Added Default Project

---
+++ DbOperationsWithEFCoreApp/DbOperationsWithEFCoreApp/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationsWithEFCoreApp.Data
+{
+    public class AppDbcontext : DbContext
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options) : base(options)  
+        {
+        }
+    }
+}
